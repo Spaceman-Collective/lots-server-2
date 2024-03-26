f96e8b4f5c273cdc99c1e16fc4b867051189fe03
spacemandev <devbharel@gmail.com>
2024-03-26 13:21:10 -0500

    added compile code to schemas

---
+++ packages/colyseus/src/schema/compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf lots-unity-schemas/
+
+for file in *.ts; do
+    # Run the command with the current file
+    schema-codegen $file --output ./lots-unity-schemas/ --csharp
+done
+
+for file in rooms/*.ts; do
+    # Run the command with the current file
+    schema-codegen $file --output ./lots-unity-schemas/rooms/ --csharp
+done
+
+zip -r lots-unity-schemas.zip lots-unity-schemas/
